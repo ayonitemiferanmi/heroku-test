a5eaf8c0fdf268e2f31dc5033175433704c0e3a7
ayonitemiferanmi <52452486+ayonitemiferanmi@users.noreply.github.com>
2025-01-23 17:56:47 +0100

    Add files via upload

--- fast_api.py
+++ fast_api.py
@@ -5,12 +5,12 @@ import numpy as np
 from ultralytics import YOLOv10
 import io
 import requests
-#import virtualenv
 import os
+import json
 
-#venv_dir = os.path.join(os.path.expanduser("~"), ".venv")
-#virtualenv.create_environment(venv_dir)
-#exec(open(os.path.join(os.path.expanduser("~"), ".venv", "Scripts", "fast_api.py")).read(), {'__file__': os.path.join(os.path.expanduser("~"), ".venv", "Scripts", "fast_api.py')})
+# venv_dir = os.path.join(os.path.expanduser("~"), ".venv")
+# virtualenv.create_environment(venv_dir)
+# exec(open(os.path.join(os.path.expanduser("~"), ".venv", "Scripts", "fast_api.py")).read(), {'__file__': os.path.join(os.path.expanduser("~"), ".venv", "Scripts", "fast_api.py')})
 
 
 app = FastAPI()
@@ -19,14 +19,13 @@ app = FastAPI()
 model_path = "best.pt"
 model = YOLOv10(model_path, task='detect')
 
-#@app.middleware("https")
 @app.get("/")
 def hello():
     return "Welcome to this fastapi"
 
-#@app.middleware("https")
 @app.post("/detect")
 async def detect_objects(file: UploadFile = File(...), confidence: float = 0.05):
+    global results
     # Read the uploaded image file
     image_bytes = await file.read()
     
@@ -38,7 +37,7 @@ async def detect_objects(file: UploadFile = File(...), confidence: float = 0.05)
     
     # Perform object detection using YOLOv10 model
     results = model(source=image_np, conf=confidence, save=False)
-    
+
     # Plot the detected objects on the image
     result_image_np = results[0].plot()  # This is a numpy array with detections
 
@@ -49,9 +48,28 @@ async def detect_objects(file: UploadFile = File(...), confidence: float = 0.05)
     img_byte_arr = io.BytesIO()
     result_image_pil.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
-    
-    # Return the image with detections as a StreamingResponse
+
+     # Return the image with detections as a StreamingResponse
     return StreamingResponse(img_byte_arr, media_type="image/png")
+
+@app.get("/json_response")
+def get_json_response() -> dict:
+    # Get the results of te detection from the detect_object function
+    detect_objects()
+
+    # Convert the results to json string
+    for result in results:
+        json_result_string = json.loads(result.tojson())
+
+    # Put everything together and add the key name for every dectected object
+    final_dict = {}
+    for i in range(len(json_result_string)):
+        final_dict[f"Detection-{i}"] = json_result_string[i]
+    
+    json_dumped_result = json.dumps(final_dict, indent=3)
+    
+    # Convert the JSON response
+    return final_dict
 
 # To run the app:
 # uvicorn fastapi_app:app --reload
